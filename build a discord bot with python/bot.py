a7d07830d011bb2aa1a4b736ef6d9cc43340dd6d
agatakul <140451545+agatakul@users.noreply.github.com>
2023-08-05 18:51:00 +0200

    bot.py

--- build a discord bot with python/bot.py
+++ build a discord bot with python/bot.py
@@ -3,10 +3,14 @@
 import discord
 import requests
 import json
+from dotenv import dotenv_values
+
 
 #requests package allows us to make HTTP requests to any URL. In this case we're calling the GET method to the URL that will give us the meme data
 
 #json package allows us to read JSON data. This is useful since most data passed around on the web is in the JSON format, like the JSON response we saw above
+config = dotenv_values(".env")
+token = config['TOKEN']
 
 def get_meme():
 	response = requests.get('https://meme-api.com/gimme')
@@ -30,7 +34,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('MTEzNjAyOTU4NDYwMTY2NTcxMA.GZnMb8.ITkJHSos9s3Yka565to44HwFH0iYMe6IlSTxDU') 
+client.run(token) 
 
 # That's all it takes to set up and code a Discord bot in Python! Just a note: the bot will respond to you as long as the program is kept running. If you close your terminal or turn off your computer, it will no longer be running. If you want to keep the program running forever, we’ll have to deploy it to another computer in the cloud. However, that is a lesson for another day.
 #
